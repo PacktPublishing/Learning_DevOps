a8b2f2259ef5ae2f5f7b06b1e8d9c613ef334982
Mikael Krief <krief_mikael@hotmail.com>
2019-04-28 11:43:57 +0200

    Add chap02 scripts

---
+++ CHAP02/terraform_vars_interp/terraform.tfvars
@@ -0,0 +1,2 @@
+resoure_group_name ="bookRg"
+application_name ="book"
---
+++ CHAP02/terraform_vars_interp/variables.tf
@@ -0,0 +1,12 @@
+variable "resoure_group_name" {
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  description = "Location of the resource"
+  default     = "West Europe"
+}
+
+variable "application_name" {
+  description = "Name of the application"
+}
