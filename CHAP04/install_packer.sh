7a1c26133c2ae3b94afc919496abc4eb036ec18c
Mikael Krief <krief_mikael@hotmail.com>
2019-05-13 08:50:53 +0200

    add packer install

---
+++ CHAP04/install_packer.sh
@@ -0,0 +1,9 @@
+PACKER_VERSION="1.4.0"
+
+curl -Os https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
+&& curl -Os https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_SHA256SUMS \
+&& curl https://keybase.io/hashicorp/pgp_keys.asc | gpg --import \
+&& curl -Os https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_SHA256SUMS.sig \
+&& gpg --verify packer_${PACKER_VERSION}_SHA256SUMS.sig packer_${PACKER_VERSION}_SHA256SUMS \
+&& shasum -a 256 -c packer_${PACKER_VERSION}_SHA256SUMS 2>&1 | grep "${PACKER_VERSION}_linux_amd64.zip:\sOK" \
+&& unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin
