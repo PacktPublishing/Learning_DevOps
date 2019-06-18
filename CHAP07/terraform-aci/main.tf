18f77ffcf551f288ca0e445c08a1c3bbfda198e1
Mikael KRIEF <mikael.krief@younited-credit.fr>
2019-06-18 14:08:17 +0200

    upgrade and clean

--- CHAP07/terraform-aci/main.tf
+++ CHAP07/terraform-aci/main.tf
@@ -1,23 +1,44 @@
 provider "azurerm" {}
 
+terraform {
+  backend "azurerm" {}
+  required_version = ">= 0.12"
+}
+
 resource "azurerm_resource_group" "acidemobook" {
   name     = "demoBook"
   location = "westus2"
 }
 
+variable "imageversion" {
+  description = "Tag of the image to deploy"
+}
+
+variable "dockerhub-username" {
+  description = "DockerHub username"
+}
+
+
 resource "azurerm_container_group" "aci-myapp" {
-  name                = "aci-agent"
+  name                = "aci-app"
   location            = "West Europe"
   resource_group_name = azurerm_resource_group.acidemobook.name
   ip_address_type     = "public"
+  dns_name_label      = "myapp-demo"
   os_type             = "linux"
-
   container {
     name   = "myappdemo"
-    image  = "docker.io/mikaelkrief/demobook:v1"
+    image  = "docker.io/${var.dockerhub-username}/demobook:${var.imageversion}"
     cpu    = "0.5"
     memory = "1.5"
-    port   = "80"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+
   }
+
+
 
 }
