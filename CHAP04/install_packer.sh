6d3f20ee940f598cd040bb31b9864c24407a2ee7
MikaelKRIEF <mikael.krief@younited-credit.fr>
2019-08-07 18:37:59 +0200

    update code

--- CHAP04/install_packer.sh
+++ CHAP04/install_packer.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-curl -Os https://releases.hashicorp.com/packer/1.4.0/packer_1.4.0_linux_amd64.zip \
-&& curl -Os https://releases.hashicorp.com/packer/1.4.0/packer_1.4.0_SHA256SUMS \
+PACKER_VERSION="1.4.0" #Update with your desired version
+
+curl -Os https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
+&& curl -Os https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_SHA256SUMS \
 && curl https://keybase.io/hashicorp/pgp_keys.asc | gpg --import \
-&& curl -Os https://releases.hashicorp.com/packer/1.4.0/packer_1.4.0_SHA256SUMS.sig \
-&& gpg --verify packer_1.4.0_SHA256SUMS.sig packer_1.4.0_SHA256SUMS \
-&& shasum -a 256 -c packer_1.4.0_SHA256SUMS 2>&1 | grep "1.4.0_linux_amd64.zip:\sOK" \
-&& unzip -o packer_1.4.0_linux_amd64.zip -d /usr/local/bin
+&& curl -Os https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_SHA256SUMS.sig \
+&& gpg --verify packer_${PACKER_VERSION}_SHA256SUMS.sig packer_${PACKER_VERSION}_SHA256SUMS \
+&& shasum -a 256 -c packer_${PACKER_VERSION}_SHA256SUMS 2>&1 | grep "${PACKER_VERSION}_linux_amd64.zip:\sOK" \
+&& unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin
